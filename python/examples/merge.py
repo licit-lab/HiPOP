e6e516a0fce73df9a863324e55570c4bef5879f4
Cécile BECARIE <cecile.becarie@ifsttar.fr>
2022-07-27 15:38:37 +0200

    Minor fixes
    added a compile option for the examples (mandatory for osx)
    Fixed import error
    Correction of the add_node function parameters

--- python/examples/merge.py
+++ python/examples/merge.py
@@ -1,13 +1,13 @@
 from importlib.resources import path
-from hipop import OrientedGraph
+from hipop.graph import OrientedGraph
 from hipop.cpp import merge_oriented_graph
 
 G = OrientedGraph()
 
-G.add_node("0", 0, 0, {})
-G.add_node("1", 1, 0, {})
-G.add_node("2", 1, 1, {})
-G.add_node("3", 0, 1, {})
+G.add_node("0", 0, 0, 'N0')
+G.add_node("1", 1, 0, 'N0')
+G.add_node("2", 1, 1, 'N0')
+G.add_node("3", 0, 1, 'N0')
 
 G.add_link("0_1", "0", "1", 1, {"time": 10})
 G.add_link("0_3", "0", "3", 1, {"time": 11})
