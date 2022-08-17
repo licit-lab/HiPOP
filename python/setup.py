f03d7574829097635b807fdbd19b3b06752f2dd7
Florian Gacon <florian.gacon@gmail.com>
2022-08-17 15:38:43 +0200

    Towards full functional ride sharing

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import sys
 import codecs
+from pathlib import Path
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
@@ -130,11 +131,11 @@ if sys.argv[1] == "bdist_wheel":
     main()
 
 
-
 def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    here = Path(__file__).parent.resolve()
+    with codecs.open(here.joinpath(rel_path), 'r') as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
@@ -143,7 +144,6 @@ def get_version(rel_path):
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
-
 
 
 # The information here can also be placed in setup.cfg - better separation of
