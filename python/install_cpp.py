6ee6d68d3f4c55d1438ddd8610680ccab544cdb8
Florian Gacon <florian.gacon@gmail.com>
2022-09-13 08:56:24 +0200

    Clean up tempdir only on unix system

--- python/install_cpp.py
+++ python/install_cpp.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 import tempfile
 from pathlib import Path
-
+import platform
 
 def build_hipop_cpp(prefix):
     cwd = os.getcwd()
@@ -11,13 +11,14 @@ def build_hipop_cpp(prefix):
     temp_dir = tempfile.TemporaryDirectory()
     os.chdir(temp_dir.name)
     try:
-        subprocess.run([fr"cmake -S {src_dir} -DCMAKE_PREFIX_PATH={prefix} -DCMAKE_INSTALL_PREFIX={prefix} -DCMAKE_BUILD_TYPE=Release"], shell=True)
-        subprocess.run([r"cmake --build . --target install --config Release"], shell=True)
+        subprocess.check_call(fr"cmake -S {src_dir} -DCMAKE_PREFIX_PATH={prefix} -DCMAKE_INSTALL_PREFIX={prefix} -DCMAKE_BUILD_TYPE=Release", shell=True)
+        subprocess.check_call(r"cmake --build . --target install --config Release", shell=True)
     except:
         print("Build failed ..")
         sys.exit(-1)
     finally:
-        temp_dir.cleanup()
+        if platform.system() != "Windows":
+            temp_dir.cleanup()
         os.chdir(cwd)
 
 
