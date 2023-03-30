71314404ab883e7c942fb8cf2d24ae3e560ac62d
Mélanie Cortina <melanie.cortina.dev@gmail.com>
2023-03-30 16:45:00 +0200

    Add parallelIntermodalDijkstra to compute the shortest path that uses at least one layer of each set of the pairMandatoryLabels.

--- python/hipop/shortest_path.py
+++ python/hipop/shortest_path.py
@@ -1,1 +1,1 @@
-from hipop.cpp.shortest_path import (dijkstra, parallel_dijkstra, k_shortest_path, parallel_k_shortest_path, astar_euclidian_dist, compute_path_length, compute_path_cost, compute_paths_costs)
+from hipop.cpp.shortest_path import (dijkstra, parallel_dijkstra, parallel_intermodal_dijkstra, k_shortest_path, parallel_k_shortest_path, astar_euclidian_dist, compute_path_length, compute_path_cost, compute_paths_costs)
